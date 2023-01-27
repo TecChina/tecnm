1088a6d39a7454f22df84c03381b71db16c9d79a
Rick <rickchanmariin17@gmail.com>
2023-01-26 19:30:59 -0600

    Update lista-jefes.php

--- usuarios/lista-jefes.php
+++ usuarios/lista-jefes.php
@@ -56,7 +56,7 @@ if (isset($_SESSION['u_usuario']) && $_SESSION['u_privilegio']  == 0 ) {
 
   <head>
     <?php include('../layout/head.php'); ?>
-    <title>Listado de Alumnos</title>
+    <title>Listado de Jef</title>
   </head>
 
   <body class="hold-transition skin-blue sidebar-mini">
