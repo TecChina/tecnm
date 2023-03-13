19399630a21e965ae8e7e36260643fd3511aff25
carlos <carlos@gmail.com>
2023-03-13 04:47:23 -0600

    RENOVACIÓN

--- usuarios/extraescolar/nuevoCiclo.php
+++ usuarios/extraescolar/nuevoCiclo.php
@@ -10,28 +10,7 @@ $correo_sesion = $_SESSION['u_usuario'];
     $query_sesion->execute();
     $sesion_usuarios = $query_sesion->fetchAll(PDO::FETCH_ASSOC);
     foreach ($sesion_usuarios as $sesion_usuario){
-        $id_sesion = $sesion_usuario['id'];
-         $id_nombres = $sesion_usuario['nombres'];
-        $id_ap_paterno = $sesion_usuario['ap_paterno'];
-        $id_ap_materno = $sesion_usuario['ap_materno'];
-         $id_sexo = $sesion_usuario['sexo'];
-         $id_numero_control = $sesion_usuario['numero_control'];
-         $id_carrera = $sesion_usuario['carrera'];
-         $id_correo = $sesion_usuario['correo'];
-         $id_estado_civil = $sesion_usuario['estado_civil'];
-         $id_telefono = $sesion_usuario['telefono'];
-         $id_ciudad = $sesion_usuario['ciudad'];
-         $id_colonia = $sesion_usuario['colonia'];
-         $id_calle = $sesion_usuario['calle'];
-         $id_codigo_postal = $sesion_usuario['codigo_postal'];
-         $id_curp = $sesion_usuario['curp'];
-         $id_fecha_nacimiento = $sesion_usuario['fecha_nacimiento'];
-         $id_nivel_escolar = $sesion_usuario['nivel_escolar'];
-         $id_reticula = $sesion_usuario['reticula'];
-         $id_entidad = $sesion_usuario['entidad'];
-         $id_foto_perfil = $sesion_usuario['foto_perfil'];
-       
-
+        include('../php/sesion/secion.php');
     }
 
 ?>
